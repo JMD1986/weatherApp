850d31591c9b3d62174e39e1f79736bac6440a0e
John-Daugherty <johndaugherty1986@gmail.com>
2019-05-04 12:40:03 -0500

    added zip code validation

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Input, Button, TextField } from "@material-ui/core";
+import {Input, Button, TextField, Modal, Typography } from "@material-ui/core";
 class PostList extends React.Component{
   constructor(props) {
     super(props);
-    this.state = { weather: '', zipCode: 37377, countryCode: 'us' };
+    this.state = { weather: '', zipCode: 37377, countryCode: 'us', open: false };
     }
     AddZipToState=(e)=>{
         this.setState({zipCode : e.target.value})
-        console.log(e.target.value)
     }
     apiKey="f6a6fec7e553937429cc047053a9c580"
     cityID=2172797;
@@ -16,7 +14,7 @@ class PostList extends React.Component{
 
     grabWeather =()=>{
     //   fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zipCode},${this.state.countryCode}}&appid=${this.apiKey}`)
-    fetch('api.openweathermap.org/data/2.5/weather?id={'+this.state.zipCode+'},{'+this.state.countryCode+'}id=f6a6fec7e553937429cc047053a9c580')
+    fetch('api.openweathermap.org/data/2.5/weather?id={'+this.state.zipCode+'},{'+this.state.countryCode+'}id=f6a6fec7e553937429cc047053a9c580&units=imperial')
     .then((response)=> {
     console.log(response);
     })
@@ -24,19 +22,26 @@ class PostList extends React.Component{
     returnState=()=>{
         console.log(this.state)
     }
-
+    switchModal = ()=>{
+        this.setState({open: !this.state.open})
+    }
     weatherBalloon=(type) =>{
-        fetch('https://api.openweathermap.org/data/2.5/forecast?zip=' + this.state.zipCode + ',' + this.state.countryCode + '&appid=' + this.apiKey)  
-        .then((resp)=> { return resp.json() }) // Convert data to json
-        .then((data) =>{
-            console.log(data.list)
-          this.setState({weather:data.list })
-          console.log(this.state.weather[0])
-
-        })
-        .catch(function() {
-          // catch any errors
-        });
+        if (this.state.zipCode.toString().length !==5){
+            this.switchModal()
+        } else {
+            fetch('https://api.openweathermap.org/data/2.5/forecast?zip=' + this.state.zipCode + ',' + this.state.countryCode + '&appid=' + this.apiKey + '&units=imperial')  
+            .then((resp)=> { return resp.json() }) // Convert data to json
+            .then((data) =>{
+                console.log(data.list)
+              this.setState({weather:data.list })
+              console.log(this.state.weather[0])
+    
+            })
+            .catch(function() {
+              // catch any errors
+            });
+        }
+   
       }
       weatherValue =()=>{
           if (this.state.weather!==''){
@@ -65,6 +70,13 @@ class PostList extends React.Component{
             <Button onClick={this.weatherBalloon}>search</Button>
             <Button onClick={this.returnState}>check stuff</Button>
             <this.weatherValue />
+            <Modal
+          
+          open={this.state.open}
+        >
+<Typography>zipcode must be 5 digits<Button onClick={this.switchModal}>Ok</Button></Typography>
+
+        </Modal>
         </div>
     )
     };
