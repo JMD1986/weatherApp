b81aa228e4c410a48a1beb3268d0e1f4490eddc3
John-Daugherty <johndaugherty1986@gmail.com>
2019-05-05 10:46:34 -0500

    added lists

--- src/components/FiveDayForecast.js
+++ src/components/FiveDayForecast.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import {Input, Button, TextField, Modal, Typography } from "@material-ui/core";
+import {Input, Button, TextField, ListItem, List, ListItemText, Avatar } from "@material-ui/core";
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import moment from 'moment';
 class FiveDayForecast extends React.PureComponent{
   constructor(props) {
     super(props);
@@ -8,8 +12,8 @@ class FiveDayForecast extends React.PureComponent{
 
     }
 returnState = ()=>{
-  console.log(this.props.weather)
-  console.log("days mapped " + this.daysToMap)
+  console.log("data" + (this.props.weather[9].dt))
+  console.log("day " + moment.unix(this.props.weather[9].dt).format("MM/DD/YYYY h:mm A"))
 }
 
 extractFiveDay = ()=>{
@@ -25,10 +29,33 @@ extractFiveDay = ()=>{
 
 
 <div>five day</div>
-      <TextField value={this.props.weather[9].weather[0].main}/>
-      <TextField value={this.props.weather[17].weather[0].main}/>
-      <TextField value={this.props.weather[25].weather[0].main}/>
-      <TextField value={this.props.weather[33].weather[0].main}/>
+     
+      <List >
+                    <ListItem>
+                        <Avatar>
+                        <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[9].main.temp} secondary={moment.unix(this.props.weather[9].dt).format("MM/DD/YYYY h:mm A")} />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <WorkIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[17].main.temp} secondary={moment.unix(this.props.weather[17].dt).format("MM/DD/YYYY h:mm A")} />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <BeachAccessIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[25].main.temp} secondary={moment.unix(this.props.weather[25].dt).format("MM/DD/YYYY h:mm A")}/>
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <BeachAccessIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[33].main.temp} secondary={moment.unix(this.props.weather[33].dt).format("MM/DD/YYYY h:mm A")} />
+                    </ListItem>
+                    </List>  
       <Button onClick={this.returnState} >check</Button>
 
       </div>
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {Input, Button, TextField, Modal, Typography } from "@material-ui/core";
+import {Input, Button, TextField, Modal, Typography, AppBar, Card, ListItem, List, ListItemText, Avatar } from "@material-ui/core";
 import FiveDayForecast from './FiveDayForecast';
 import { connect } from 'react-redux';
 import { fetchWeather } from '../actions';
-
-class PostList extends React.Component{
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import SimpleAppBar from './appBar';
+class WeatherList extends React.Component{
   constructor(props) {
     super(props);
     this.state = { weather: '', zipCode: 37377, countryCode: 'us', open: false };
     }
-    AddZipToState=(e)=>{
+    AddZipToState = (e) => {
         this.setState({zipCode : e.target.value})
     }
-    weatherSearch = ()=>{
+    weatherSearch = () => {
         if (this.state.zipCode.toString().length!==5){
             this.switchModal()
         } else {
@@ -31,27 +34,53 @@ class PostList extends React.Component{
           if (this.props.weather[0]){
             return <div>
                 <div>today</div>
-                <TextField value={this.props.weather[0].weather[0].main}/>
-                <TextField value={this.props.weather[0].main.temp}/>
-                <TextField value={this.props.weather[0].main.temp_min}/>
-                <TextField value={this.props.weather[0].main.temp_max}/>             
+             
+                <List >
+                    <ListItem>
+                        <Avatar>
+                        <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[0].weather[0].main} secondary="Weather" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <WorkIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[0].main.temp} secondary="current Temperature" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <BeachAccessIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[0].main.temp_max} secondary="Todays High" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                        <BeachAccessIcon />
+                        </Avatar>
+                        <ListItemText primary={this.props.weather[0].main.temp_min} secondary="Todays Low" />
+                    </ListItem>
+                    </List>        
             </div>
             }else {
           return null
         }
       }
-    //   componentDidMount(){
-    //       this.props.fetchWeather();
-    //   }
+  
     render (){
-        console.log(this.props.weather[0])
     return(
         <div>
+            <AppBar >WeatherFinder</AppBar>
+            <div style={{marginTop :20}}> 
+            <Card> 
             <Input value={this.state.zipCode} onChange={this.AddZipToState}/>
             <Button onClick={this.weatherSearch}>search</Button>
             <Button onClick={this.returnState}>check stuff</Button>
+
+            </Card>
             <this.weatherValue />
             <FiveDayForecast weather={this.props.weather} />
+            </div>
             <Modal
           
           open={this.state.open}
@@ -68,4 +97,4 @@ const mapStateToProps = (state) => {
  return { weather : state.weather}
 }
 
-export default connect(mapStateToProps, {fetchWeather})(PostList);
+export default connect(mapStateToProps, {fetchWeather})(WeatherList);
